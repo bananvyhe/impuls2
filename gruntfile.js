55b37d0f2f1423136e933c88793a689d53374523
bananvyhe <loadonden@yahoo.com>
2017-04-13 22:44:30 +0500

    ++

---
+++ gruntfile.js
@@ -0,0 +1,37 @@
+module.exports = function(grunt) {  
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+    grunt.loadNpmTasks('grunt-contrib-watch');
+    grunt.loadNpmTasks('grunt-contrib-compass');
+    
+    grunt.initConfig({
+      uglify: {
+        my_target: {
+          files: {
+            'app/assets/javascripts/script.js': ['app/assets/javascripts/*.js']
+          } //files
+        } //my_target
+      }, //uglify
+      compass: {
+        dev: {
+          options: {
+            config: 'config.rb'
+          } //options
+        } //dev
+      }, //compass
+      watch: {
+        options: { livereload: true },
+        scripts: {
+          files: ['app/assets/javascripts/*.js'],
+          tasks: ['uglify']
+        }, //script
+        sass: {
+          files: ['app/assets/stylesheets/*.scss'],
+          tasks: ['compass:dev']
+        }, //sass
+        html: {
+          files: ['**/*.html.erb']
+        }
+      } //watch
+    }) //initConfig
+    grunt.registerTask('default', 'watch');
+} //exports
